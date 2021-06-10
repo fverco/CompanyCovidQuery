302d9ac9d4d8a19a111f5fb86a2d360d15a81ee2
fverco <rapidspitfires@gmail.com>
2021-06-11 00:42:46 +0200

    Implement the surveyDb's table model

--- src/forms/mainwindow.cpp
+++ src/forms/mainwindow.cpp
@@ -1,14 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSqlTableModel>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow),
+      surveyDb()
 {
     ui->setupUi(this);
 
     if (!surveyDb.createDatabase())
         QApplication::quit();
+
+    ui->tableSurveys->setModel(surveyDb.getSurveyModel());
 }
 
 MainWindow::~MainWindow()
