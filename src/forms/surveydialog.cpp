6075781aa0a314cc9509c4a4f77d692d2eeccbaf
fverco <francov2468@gmail.com>
2021-07-10 21:38:33 +0200

    Overhaul SurveyDialog and add edit survey functionality

--- src/forms/surveydialog.cpp
+++ src/forms/surveydialog.cpp
@@ -4,30 +4,21 @@
 #include <QPushButton>
 #include <QButtonGroup>
 #include <QMessageBox>
-#include <QDate>
 
 /*!
  * \brief The constructor for the SurveyDialog.
  * \param parent = The QWidget to which this dialog is bound to.
  */
-SurveyDialog::SurveyDialog(const int& emp, QWidget *parent) :
+SurveyDialog::SurveyDialog(const int& emp, QWidget *parent, const Survey &editSurvey) :
     QDialog(parent),
     ui(new Ui::SurveyDialog),
     empId(emp),
     rbgroupQOne(new QButtonGroup(this)),
     rbgroupQTwo(new QButtonGroup(this)),
-    rbgroupQThree(new QButtonGroup(this))
+    rbgroupQThree(new QButtonGroup(this)),
+    editMode(false)
 {
     ui->setupUi(this);
-
-    // Set the "OK" button text to "Add" and disable it.
-    ui->buttonBox->button(QDialogButtonBox::Ok)->setText("Add");
-    ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(true);
-
-    // Connect the "toggled" signals of all button groups with the questionAnswered() function.
-    connect(rbgroupQOne.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
-    connect(rbgroupQTwo.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
-    connect(rbgroupQThree.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
 
     // Add all the "Yes" and "No" radio buttons to their respective button groups.
     rbgroupQOne->addButton(ui->rbQOneYes);
@@ -37,9 +28,11 @@ SurveyDialog::SurveyDialog(const int& emp, QWidget *parent) :
     rbgroupQThree->addButton(ui->rbQThreeYes);
     rbgroupQThree->addButton(ui->rbQThreeNo);
 
-    // Set the default date to today and give the date edit a calendar popup.
-    ui->cbTodayDate->setCheckState(Qt::CheckState::Checked);
-    ui->deSurveyDate->setCalendarPopup(true);
+    // If a survey is about to be edited instead...
+    if (editSurvey.isValid())
+        setupEditMode(editSurvey);
+    else
+        setupAddMode();
 }
 
 /*!
@@ -51,27 +44,8 @@ SurveyDialog::~SurveyDialog()
 }
 
 /*!
- * \brief Determines whether Add or Cancel is clicked and performs the appropriate action.
- * \param button = The button that was clicked
- */
-void SurveyDialog::on_buttonBox_clicked(QAbstractButton *button)
-{
-    if (button->text() == "Add") {
-
-        QDate survDate(ui->deSurveyDate->date());
-        bool qOne(rbgroupQOne->checkedButton()->text() == "Yes"),
-             qTwo(rbgroupQTwo->checkedButton()->text() == "Yes"),
-             qThree(rbgroupQThree->checkedButton()->text() == "Yes");
-        double temp(ui->dspinTemp->value());
-
-        emit sendNewSurvey(survDate, empId, qOne, qTwo, qThree, temp);
-
-    } else if (button->text() == "Cancel")
-        this->close();
-}
-
-/*!
  * \brief Whenever a radio button is toggled (question is answered) it will determine if all questions is answered and enable the Add button if true.
+ * \note This function is used only if the SurveyDialog is adding a new survey.
  */
 void SurveyDialog::questionAnswered()
 {
@@ -95,3 +69,99 @@ void SurveyDialog::on_cbTodayDate_stateChanged(int arg1)
     } else
         ui->deSurveyDate->setEnabled(true);
 }
+
+/*!
+ * \brief This function will enable the "Add" or "Apply Changes" button if any of the answers in the survey is changed by the user.
+ * \note This function is used only if the SurveyDialog is updating an existing survey.
+ */
+void SurveyDialog::answerChanged()
+{
+    if (!ui->buttonBox->button(QDialogButtonBox::Ok)->isEnabled())
+        ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(false);
+}
+
+/*!
+ * \brief Setup all UI elements for adding a new survey.
+ */
+void SurveyDialog::setupAddMode()
+{
+    // Connect the "toggled" signals of all button groups with the questionAnswered() function.
+    connect(rbgroupQOne.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
+    connect(rbgroupQTwo.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
+    connect(rbgroupQThree.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::questionAnswered);
+
+    // Set the default date to today and give the date edit a calendar popup.
+    ui->cbTodayDate->setCheckState(Qt::CheckState::Checked);
+    ui->deSurveyDate->setCalendarPopup(true);
+
+    // Set the "OK" button text to "Add" and disable it.
+    ui->buttonBox->button(QDialogButtonBox::Ok)->setText("Add");
+    ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(true);
+}
+
+/*!
+ * \brief Setup all UI elements for editing an existing survey.
+ * \param editSurvey = The survey to be edited
+ * \note This does not check if the survey is valid.
+ */
+void SurveyDialog::setupEditMode(const Survey &editSurvey)
+{
+    // Set the default date to today and give the date edit a calendar popup.
+    ui->cbTodayDate->setCheckState(Qt::CheckState::Unchecked);
+    ui->cbTodayDate->setDisabled(true);
+    ui->deSurveyDate->setDate(editSurvey.getSurveyDate());
+    ui->deSurveyDate->setDisabled(true);
+
+    // Set all answers to "No" by default.
+    ui->rbQOneNo->setChecked(true);
+    ui->rbQTwoNo->setChecked(true);
+    ui->rbQThreeNo->setChecked(true);
+
+    // Set the "OK" button text to "Add" and disable it.
+    ui->buttonBox->button(QDialogButtonBox::Ok)->setText("Apply Changes");
+    ui->buttonBox->button(QDialogButtonBox::Ok)->setDisabled(true);
+
+    // Assign the answers of the survey to the UI.
+    // NOTE: All answers MUST be set to "No" beforehand for this to work.
+    ui->rbQOneYes->setChecked(editSurvey.getQuestionOne());
+    ui->rbQTwoYes->setChecked(editSurvey.getQuestionTwo());
+    ui->rbQThreeYes->setChecked(editSurvey.getQuestionThree());
+    ui->dspinTemp->setValue(editSurvey.getTemperature());
+
+    // Connect the "toggled" and "valueChanged" signals of questions with the answerChanged() function.
+    connect(rbgroupQOne.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::answerChanged);
+    connect(rbgroupQTwo.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::answerChanged);
+    connect(rbgroupQThree.get(), &QButtonGroup::buttonToggled, this, &SurveyDialog::answerChanged);
+    connect(ui->dspinTemp, &QDoubleSpinBox::valueChanged, this, &SurveyDialog::answerChanged);
+
+    this->setWindowTitle(tr("Editing survey"));
+
+    editMode = true;
+}
+
+/*!
+ * \brief If the "Add" or "Apply Changes" is clicked, the survey will be added or updated.
+ */
+void SurveyDialog::on_buttonBox_accepted()
+{
+    Survey survey(ui->deSurveyDate->date(),
+                     empId,
+                     rbgroupQOne->checkedButton()->text() == "Yes",
+                     rbgroupQTwo->checkedButton()->text() == "Yes",
+                     rbgroupQThree->checkedButton()->text() == "Yes",
+                     ui->dspinTemp->value());
+
+    if (editMode)
+        emit updateSurvey(survey);
+    else
+        emit sendNewSurvey(survey);
+}
+
+/*!
+ * \brief If the "Cancel" button is clicked, the window will be closed.
+ */
+void SurveyDialog::on_buttonBox_rejected()
+{
+    this->close();
+}
+
--- src/forms/surveydialog.h
+++ src/forms/surveydialog.h
@@ -1,6 +1,8 @@
 #ifndef SURVEYDIALOG_H
 #define SURVEYDIALOG_H
 
+#include "../objects/survey.h"
+
 #include <QDialog>
 #include <QAbstractButton>
 
@@ -16,22 +18,19 @@ class SurveyDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit SurveyDialog(const int& emp, QWidget *parent = nullptr);
+    explicit SurveyDialog(const int& emp, QWidget *parent = nullptr, const Survey &editSurvey = Survey());
     ~SurveyDialog();
 
 signals:
-    void sendNewSurvey(const QDate& date,
-                       const int& empId,
-                       const bool& qOne,
-                       const bool& qTwo,
-                       const bool& qThree,
-                       const double& temp);
+    void sendNewSurvey(const Survey &newSurvey);
+    void updateSurvey(const Survey &survey);
 
 private slots:
-    void on_buttonBox_clicked(QAbstractButton *button);
     void questionAnswered();
-
     void on_cbTodayDate_stateChanged(int arg1);
+    void answerChanged();
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
 
 private:
     Ui::SurveyDialog *ui;   ///< The reference to the UI of the SurveyDialog.
@@ -39,6 +38,10 @@ private:
     QSharedPointer<QButtonGroup> rbgroupQOne;   ///< The button group for the question 1 radio buttons.
     QSharedPointer<QButtonGroup> rbgroupQTwo;   ///< The button group for the question 2 radio buttons.
     QSharedPointer<QButtonGroup> rbgroupQThree; ///< The button group for the question 3 radio buttons.
+    bool editMode;  ///< Is the dialog in edit mode?
+
+    void setupAddMode();
+    void setupEditMode(const Survey &editSurvey);
 };
 
 #endif // SURVEYDIALOG_H
